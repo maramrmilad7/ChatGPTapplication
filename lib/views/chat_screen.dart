b09bcb12bd3e6a0b1ed8b062033071f6b3393197
maramrmilad7 <maro12787@gmail.com>
2025-10-23 21:13:28 +0200

    create the ui without the chat widget

--- lib/views/chat_screen.dart
+++ lib/views/chat_screen.dart
@@ -1,5 +1,7 @@
+import 'package:chatgptapplication/constants/constants.dart';
 import 'package:chatgptapplication/services/assets_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,12 +11,27 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+final  bool _isTyping = true;
+late TextEditingController _textController;
+@override
+  void initState() {
+    _textController = TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose(){
+    _textController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
-        title: Text('Chat GPT', style: TextStyle(color: Colors.white)),
+        title: Text(
+          'ChatGPT',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Image.asset(AssetsManager.openaiLogo),
@@ -25,6 +42,53 @@ class _ChatScreenState extends State<ChatScreen> {
             icon: Icon(Icons.more_vert_rounded, color: Colors.white),
           ),
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text('Message '));
+                },
+              ),
+            ),
+
+            if(_isTyping) ...[
+            const  SpinKitThreeBounce(
+                color: Colors.white,
+                size: 18,
+              ), 
+            ],
+            SizedBox(height: 15,),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onSubmitted: (value) {},
+                        controller: _textController,
+                        style: TextStyle(color: Colors.white),//color of input text
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type a message',
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.send_rounded, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
