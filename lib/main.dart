e1549e4283c29cc4dbcc2a75ba076d125bf85f1d
maramrmilad7 <maro12787@gmail.com>
2025-10-23 18:44:07 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatGptApp());
+}
+class ChatGptApp extends StatelessWidget {
+  const ChatGptApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp();
+  }
+}
